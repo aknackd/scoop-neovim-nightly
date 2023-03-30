996353cf53956e7a8c140eb87a6c2882357861e0
aknackd <adam@pyknotic.net>
2023-03-29 21:10:27 -0500

    feat: Append the target commitish to the version in the manifest
    The version number in the manifest should match the version reported in
    the first line from "nvim --version," but unfortunately we can't get the
    number of commits since the last stable release unless we keep a local
    copy of the git repo. Instead we just fetch the nightly release's
    target commitish and include that in the version.

--- generate-manifest.ps1
+++ generate-manifest.ps1
@@ -3,7 +3,39 @@ param (
     [string] $OutputFile = "neovim-nightly.json"
 )
 
-# Fetch and return the SHA256 checkusm of the current nightly release
+$GitHubApiHeaders = @{
+    'Accept' = 'application/vnd.github+json'
+    'X-GitHub-Api-Version' = '2022-11-28'
+}
+
+# Fetches the nightly release ID from the "nightly" tag so we can later fetch
+# the commitish to append to the version number in the manifest.
+function Get-Nightly-Release-ID () {
+    $url = "https://api.github.com/repos/neovim/neovim/releases/tags/nightly"
+
+    $response = Invoke-RestMethod    `
+        -Uri "$url"                  `
+        -Method Get                  `
+        -Headers $GitHubApiHeaders
+
+    return $response.id
+}
+
+# Fetches the commitish to append to the version in the manifest.
+function Get-Nightly-Release-Commitsh () {
+    $releaseId = Get-Nightly-Release-ID
+
+    $releaseUrl = "https://api.github.com/repos/neovim/neovim/releases/${releaseId}"
+
+    $response = Invoke-RestMethod  `
+        -Uri $releaseUrl           `
+        -Method Get                `
+        -Headers $GitHubApiHeaders
+
+    return $response.target_commitish
+}
+
+# Fetch and return the SHA256 checkusm of the current nightly release.
 function Fetch-Nightly-Release-Checksum () {
     Write-Host "Fetching nightly release checksum"
 
@@ -33,17 +65,26 @@ function Fetch-Nightly-Release-Checksum () {
     }
 }
 
-# Builds the scoop manifest
-function Build-Manifest () {
-    Write-Host "Building manifest"
+# Generates the scoop manifest.
+function Generate-Scoop-Manifest () {
+    # Extract the semver from the global -Version param and add the release
+    # commitish so we get a unique version. Unfortunately we can't get the
+    # number of commits since the last stable release like when neovim reports
+    # on startup and on the first line of output from  "nvim --version" to get
+    # a matching version.
+
+    $commitish = $(Get-Nightly-Release-Commitsh).Substring(0, 9)
+
+    $sanitizedVersion = $Version -replace "-.+$", ""
+    $fullVersion = "${sanitizedVersion}-dev+g${commitish}"
+
+    Write-Host "Generating Scoop manifest for ${fullVersion}"
 
     $nightlyChecksum = Fetch-Nightly-Release-Checksum
 
-    $sanitizedVersion = $Version -replace "-.+$", ""
-
-    $manifest = @"
+    @"
 {
-    "version": "${sanitizedVersion}-nightly",
+    "version": "${fullVersion}",
     "description": "Vim-fork focused on extensibility and usability",
     "homepage": "https://neovim.io/",
     "license": {
@@ -74,12 +115,10 @@ function Build-Manifest () {
         "github": "https://github.com/neovim/neovim"
     }
 }
-"@
-
-    $manifest | Out-File -FilePath "$OutputFile"
+"@ | Out-File -FilePath "$OutputFile"
 }
 
-Build-Manifest
+Generate-Scoop-Manifest
 
 Write-Host "Wrote manifest to " -NoNewLine
 Write-Host -ForegroundColor Blue "$OutputFile"
